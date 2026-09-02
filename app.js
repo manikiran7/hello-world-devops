d126acd942290d0c055b4482ec412f58119840ad
manikiran <manikiranreddy7@gmail.com>
2026-09-02 12:50:59 +0530

    Fix route definition in app.js

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (_req, res) => {
+app.get("", (_req, res) => {
 	res.send("Hello World!");
 });
 
 module.exports = app;
+
+
+
+
+
+
+
+
